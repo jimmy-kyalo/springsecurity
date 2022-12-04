f7667cbea92e59705dfaebdf8aaaebe05d8e30dc
jimmy-kyalo <kyalo90@gmail.com>
2022-12-04 15:07:12 +0300

    bearer token

--- src/main/java/com/example/springsecurity/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/ApplicationSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.springsecurity;
 
+import com.example.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 // import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,18 +15,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 // import org.springframework.security.core.userdetails.User;
 // import org.springframework.security.core.userdetails.UserDetails;
 // import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 // import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.springsecurity.auth.ApplicationUserService;
 
 import static com.example.springsecurity.ApplicationUserRole.*;
 // import static com.example.springsecurity.ApplicationUserPermission.*;
 
-import java.util.concurrent.TimeUnit;
+// import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -41,11 +46,24 @@ public class ApplicationSecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // AuthenticationManager authenticationManager =
+        // http.getSharedObject(AuthenticationConfigurationn.class);
         http
                 .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 // .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 // .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(
+                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))))
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
@@ -58,27 +76,27 @@ public class ApplicationSecurityConfig {
                 // .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(),
                 // ADMIN_TRAINEE.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                // .httpBasic();
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true)
-                .passwordParameter("password")
-                .usernameParameter("username")
-                .and()
-                .rememberMe()
-                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                .key("somethingverysecured")
-                .rememberMeParameter("remember-me")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/login");
+                .authenticated();
+        // .and()
+        // // .httpBasic();
+        // .formLogin()
+        // .loginPage("/login").permitAll()
+        // .defaultSuccessUrl("/courses", true)
+        // .passwordParameter("password")
+        // .usernameParameter("username")
+        // .and()
+        // .rememberMe()
+        // .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+        // .key("somethingverysecured")
+        // .rememberMeParameter("remember-me")
+        // .and()
+        // .logout()
+        // .logoutUrl("/logout")
+        // .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+        // .clearAuthentication(true)
+        // .invalidateHttpSession(true)
+        // .deleteCookies("JSESSIONID", "remember-me")
+        // .logoutSuccessUrl("/login");
 
         return http.build();
     }
--- src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,17 +1,23 @@
 package com.example.springsecurity.jwt;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+// import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+// import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -39,4 +45,21 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult)
+            throws IOException, ServletException {
+        String key = "securesecuresecuresecuresecuresecuresecuresecure";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
 }
