43ee39afe10d1f6990d59ac0a5e34827be08e8f2
jimmy-kyalo <kyalo90@gmail.com>
2022-12-11 13:21:56 +0300

    token verifier

---
+++ src/main/java/com/example/springsecurity/auth/ApplicationUserDaoService.java
@@ -0,0 +1,17 @@
+// package com.example.springsecurity.auth;
+
+// import java.util.Optional;
+
+// public class ApplicationUserDaoService implements ApplicationUserDao {
+
+// public ApplicationUserDaoService() {
+// }
+
+// @Override
+// public Optional<ApplicationUser> selectApplicationUserByName(String username)
+// {
+// // TODO Auto-generated method stub
+// return Optional.empty();
+// }
+
+// }
